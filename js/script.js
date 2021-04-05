65b2c181da34b67796869d74c5a5f1ba1ac6987a
Ilias <ilias_98_22@inbox.ru>
2021-04-05 14:07:19 +0600

    if for

--- js/script.js
+++ js/script.js
@@ -8,13 +8,28 @@ const personalMovieDB = {
    privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+   const a = prompt('Один из последних просмотренных фильмов?', ''),
+         b = prompt('На сколько оцените его?', '');
+   
+   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log("done");
+   } else {
+      console.log('error');
+      i--;
+   }
+};
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+   console.log('Просмотрено мало филмьов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   console.log('вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+   console.log('Да вы киноман!');
+} else {
+   console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
 
