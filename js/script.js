ec077d67495f99908698a09f5452d4c478ed52b2
Ilias <ilias_98_22@inbox.ru>
2021-04-05 10:51:37 +0600

    numberOfFilms

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const  numberOfFilms = +prompt('Сколько филмов вы уже смотрели?','');
+
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
